39d787807521b5e3259e747d56d935df451ff48c
bhishm1804 <bhishm.999786@gmail.com>
2025-09-01 17:37:23 +0000

    Conversoin

---
+++ .devcontainer/01_Basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";// treat all JS code as newer version
+
+// alert(3+3) we're using node js, not browser
+
+console.log(3+3)
+
+console.log("Bhishm")
+
+let name ="Bhishm"
+let age = 27
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string =>
+// boolean => true/false
+// null
+// undefined =>
+// symbol unique
+// object
+
+console.log(typeof undefined);
+console.log(typeof null);
---
+++ .devcontainer/01_Basics/03_conversionOperation.js
@@ -0,0 +1,17 @@
+let score = "bhishm"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33"  => 33
+//"33abc" => NaN
+//true =>1; false =>0
+
+let isLoggedIn = 0
+let booleabIsLoggedIN =Boolean(isLoggedIn)
+console.log(booleabIsLoggedIN);
